ce620967864fb1c7e70b84c07d8b99c6ced66fa7
michael <michael@mholzinger.at>
2018-09-10 22:53:33 +0200

    search bar hinzugefügt

--- TodoExercise/Controlers/ToDoViewController.swift
+++ TodoExercise/Controlers/ToDoViewController.swift
@@ -31,7 +31,7 @@ class ToDoListViewController: UITableViewController {
         
     }
     
-    //MARK - Tableview Datasouce
+    //MARK: - Tableview Datasouce
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -50,7 +50,7 @@ class ToDoListViewController: UITableViewController {
   
     
     
-    //MARK - Table view delegate methoden (Events)
+    //MARK: - Table view delegate methoden (Events)
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(tableView.cellForRow(at: indexPath)?.textLabel?.text)
@@ -67,7 +67,7 @@ class ToDoListViewController: UITableViewController {
         
     }
     
-    //MARK - add button
+    //MARK: - add button
     
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -104,7 +104,7 @@ class ToDoListViewController: UITableViewController {
         
     }
     
-    //MARK - Speichern/laden Routine
+    //MARK: - Speichern/laden Routine
     
     func saveItems(){
         
@@ -116,13 +116,45 @@ class ToDoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems(){
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(_ request : NSFetchRequest<Item> = Item.fetchRequest()){
+        
         do {
             itemArray = try context.fetch(request)
         } catch {
              print("Erros core data read: \(error)")
         }
+        tableView.reloadData()
     }
+    
+}
+
+
+//MARK: - searchbar funktionalität
+
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        let preidcate = NSPredicate(format: "titel CONTAINS[cd] %@", searchBar.text!)
+        request.predicate = preidcate
+        
+        let sortierung = NSSortDescriptor(key: "titel", ascending: true)
+        request.sortDescriptors = [sortierung]
+        
+        loadItems(request)
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if (searchBar.text?.count == 0) {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+    
 }
 
